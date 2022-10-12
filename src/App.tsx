18d85843cd8864389deff650bf98fb64afece6a7
Aleksey Zubov <a.zubov@rambler-co.ru>
2022-10-12 18:24:10 +0400

    correct webpack and eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
+
 function App() {
   return <div>New React App</div>;
 }
 
 export default App;
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production'
+};
 
 module.exports = {
   entry: './src/index.tsx',
-  mode: 'development',
+  mode,
+  devtool: 'source-map',
   output: {
-    filename: 'bundle.[fullhash].js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext][query]',
+    clean: true
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css', // Формат имени файла
     }),
   ],
   resolve: {
@@ -19,20 +30,33 @@ module.exports = {
   },
   module: {
     rules: [
+      { 
+        test: /\.(html)$/, 
+        use: ['html-loader'] 
+      },
       {
         test: /.(js|ts)x?$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
       {
-        test: /.css$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
       },
       {
-        test: /.(png|svg|jpg|gif)$/,
-        exclude: /node_modules/,
-        use: ['file-loader'],
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: mode === 'production' ? 'asset' : 'asset/resource', // В продакшен режиме
+        // изображения размером до 8кб будут инлайнится в код
+        // В режиме разработки все изображения будут помещаться в dist/assets
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
